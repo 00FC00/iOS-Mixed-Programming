ad51b79f72a8d1e9730d9a4c3b00329471e9e8a9
哲思 <595240129@qq.com>
2024-07-22 17:42:12 +0800

    删除掉umbrella header “MyFramework.h”这个暴露出来的文件，效仿App应用通过MyFramework-Bridging-Header.h调用OC

---
+++ MyFramework/MyFramework/MyFramework-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  MyFramework-Bridging-Header.h
+//  MyFramework
+//
+//  Created by 哲思 on 2024/7/22.
+//
+
+#ifndef MyFramework_Bridging_Header_h
+#define MyFramework_Bridging_Header_h
+
+#import "OCTest.h"
+#endif /* MyFramework_Bridging_Header_h */
